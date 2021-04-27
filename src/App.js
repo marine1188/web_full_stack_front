9355788d50dd0ad1f203f0c36b2939b9dc031834
SEONG KYU CHO (조성규) <38072336+marine1188@users.noreply.github.com>
2021-04-27 09:30:28 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>fuck you 사랑 해요 나를 아주 많이 사랑해요 나 자신에게</p>
+    </div>
+  );
+}
